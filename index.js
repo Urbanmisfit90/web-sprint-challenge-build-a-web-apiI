41b8b3969164039d10aba3872277f66fd6c1888a
Robert Dixon <robmsc24@gmail.com>
2024-03-19 22:02:20 -0400

    Update my changes

--- index.js
+++ index.js
@@ -12,3 +12,20 @@ I need this code, but don't know where, perhaps should make some middleware, don
 
 Pull your server into this file and start it!
 */
+const express = require("express");
+const projectsRouter = require("./projects-router");
+const { getProjectActionsMiddleware } = require("./actions-middleware");
+
+const app = express();
+
+
+app.use("/api/projects/:id/actions", getProjectActionsMiddleware);
+
+
+app.use("/api/projects", projectsRouter);
+
+
+const PORT = process.env.PORT || 9000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
